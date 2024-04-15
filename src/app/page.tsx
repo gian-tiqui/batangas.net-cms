fa95911ad0f6e0e33206d15af58afbdd617af50c
gian-tiqui <michaelgiantiqui3@gmail.com>
2024-04-15 15:44:40 +0800

    new smth in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,16 +10,21 @@ const Home = async () => {
   const newsData: NewsInterface[] = response.data;
 
   return (
-    <div className="grid grid-cols-5 h-screen">
-      <div className="col-span-1 border shadow-2xl"></div>
-      <div className="col-span-4 overflow-auto p-10">
-        <button className="bg-indigo-500 hover:bg-indigo-700 rounded px-5 py-2 text-white font-bold mb-3">
-          Add news
-        </button>
-        <div className="flex flex-wrap gap-3">
-          {newsData.map((news) => (
-            <NewsCard key={news.id} {...news} />
-          ))}
+    <div className="h-screen">
+      <div className="h-16 grid grid-cols-5">
+        <div className="col-span-5 shadow"></div>
+      </div>
+      <div className="grid grid-cols-5">
+        <div className="col-span-1 shadow-2xl border"></div>
+        <div className="col-span-4 overflow-auto p-10">
+          <button className="bg-indigo-500 hover:bg-indigo-700 rounded px-5 py-2 text-white font-bold mb-3">
+            Add news
+          </button>
+          <div className="flex flex-wrap gap-3">
+            {newsData.map((news) => (
+              <NewsCard key={news.id} {...news} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
